b90fea9a1378f452b6c168bb6e80ec104836d1dc
Bartek Telec <bartektelec@gmail.com>
2023-05-28 14:57:47 +0200

    scaffold minimalistic app

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -3,10 +3,9 @@
 // but some rules are too "annoying" or are not applicable for your case.)
 #![allow(clippy::wildcard_imports)]
 
+use reqwest;
 use seed::{prelude::*, *};
-
 mod counter;
-mod input;
 
 // ------ ------
 //     Init
@@ -17,6 +16,11 @@ fn init(_: Url, _: &mut impl Orders<Msg>) -> Model {
     Model {
         counters: (0..3).map(|_| counter::init()).collect(),
         text: "".to_string(),
+        login_form: LoginForm {
+            username: String::new(),
+            password: String::new(),
+        },
+        user_data: None,
     }
 }
 
@@ -24,10 +28,27 @@ fn init(_: Url, _: &mut impl Orders<Msg>) -> Model {
 //     Model
 // ------ ------
 
-// `Model` describes our app state.
+struct LoginForm {
+    username: String,
+    password: String,
+}
+
+struct Account {
+    account_number: String,
+    balance: String,
+}
+
+struct User {
+    username: String,
+    id: i32,
+    accounts: Vec<Account>,
+}
+
 struct Model {
     counters: Vec<counter::Model>,
     text: String,
+    user_data: Option<User>,
+    login_form: LoginForm,
 }
 
 // ------ ------
@@ -40,13 +61,102 @@ type CounterId = usize;
 enum Msg {
     Counter(counter::Msg, CounterId),
     InputText(String),
+    SubmitLogin,
+    Logout,
 }
 
+fn login_form(model: &Model) -> Node<Msg> {
+    if let Some(user) = &model.user_data {
+        return div![
+            format!("You are logged in as {}", user.username),
+            button!["Log out", ev(Ev::Click, |_| Msg::Logout)]
+        ];
+    } else {
+        div![
+            label![
+                plain!["Username"],
+                input![attrs![
+                    At::Type => "text"
+                ]]
+            ],
+            label![
+                plain!["Password"],
+                input![attrs![
+                    At::Type => "password"
+                ]]
+            ],
+            button![ev(Ev::Click, |_| Msg::SubmitLogin), "Login"]
+        ]
+    }
+}
+
+fn transfer_box(user_data: &User) -> Node<Msg> {
+    div![
+        h2!["Transfer"],
+        label![
+            "Transfer from account",
+            select![user_data
+                .accounts
+                .iter()
+                .map(|acc| { option![format!("{} : ${}", acc.account_number, acc.balance)] })]
+        ],
+        label![
+            "Transfer to account",
+            input![attrs![
+                At::Type => "number"
+            ],]
+        ],
+        button!["Send"]
+    ]
+}
+
+fn account_history(user: &User) -> Node<Msg> {
+    div![
+        h2!["Account history"],
+        ul![
+            li!["Sent $20 to 10202321321"],
+            li!["Received $50 from 12313213231"]
+        ]
+    ]
+}
+
+fn app_body(model: &Model) -> Node<Msg> {
+    if let Some(user) = &model.user_data {
+        return div![
+            transfer_box(user),
+            h1!["Here are your accounts:"],
+            ul![user
+                .accounts
+                .iter()
+                .map(|acc| { li![format!("{} --- {}", acc.account_number, acc.balance)] })],
+            account_history(user)
+        ];
+    } else {
+        empty![]
+    }
+}
+
+fn perform_login_user(model: &mut Model) {
+    model.user_data = Some(User {
+        id: 1,
+        username: "bartek".to_string(),
+        accounts: vec![Account {
+            account_number: "10001123321".to_string(),
+            balance: "20".to_string(),
+        }],
+    })
+}
+
+fn perform_logout(model: &mut Model) {
+    model.user_data = None
+}
 // `update` describes how to handle each `Msg`.
 fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
     match msg {
         Msg::Counter(msg, id) => counter::update(msg, &mut model.counters[id]),
         Msg::InputText(in_text) => model.text = in_text,
+        Msg::SubmitLogin => perform_login_user(model),
+        Msg::Logout => perform_logout(model),
     }
 }
 
@@ -56,13 +166,7 @@ fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
 
 // `view` describes what to display.
 fn view(model: &Model) -> Node<Msg> {
-    div![
-        div![&model.text],
-        input::view("".to_string()),
-        model.counters.iter().enumerate().map(|(id, model)| {
-            counter::view(*model).map_msg(move |counter_msg| Msg::Counter(counter_msg, id))
-        })
-    ]
+    div![login_form(model), app_body(model)]
 }
 
 // ------ ------
